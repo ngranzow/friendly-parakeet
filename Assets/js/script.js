6e43bb04cf37f622f504bdc5c9005aeb1d827a5e
ngranzow <nate.granzow@gmail.com>
2022-01-15 17:59:25 -0600

    length prompt

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,23 @@ var uppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 var numbericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
 
+var chooseLength = function() {
+  var charLength = window.prompt("How many characters would you like? Please type number between 8-128.");
+
+  if (charLength < 8 || charLength > 8 || lowercaseChar || uppercaseChar || specialChar) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return charLength();
+  }
+};
+
+var chooseLower = function() {
+  var 
+}
+
+var generatePassword = function () {
+  console.log("Password Generated")
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,8 +31,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// writePassword();
